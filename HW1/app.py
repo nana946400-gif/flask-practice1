1e6f3afa76138ab9ee3163e3c69340edceb930ce
김난이 <24013789@sju.ac.kr>
2026-09-20 23:32:58 +0900

    Add profile page

--- HW1/app.py
+++ HW1/app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/profile")
+def profile():
+    hobbies = ["헬스", "음악 듣기", "회화 공부"]
+    return render_template("profile.html", hobbies=hobbies)
